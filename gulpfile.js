331c491b207850599832077678d0bc7b1dfec2e3
sengemann <scott.engemann@proquest.com>
2021-12-21 17:20:04 -0500

    Updating gulp tasks, fixing linking, fixing build issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass')(require('sass'));
+var clean = require('gulp-clean');
+var wait = require('gulp-wait');
 //var autoprefixer = require('gulp-autoprefixer'); 
 // requires npm install --save-dev gulp-autoprefixer
 var sourcemaps = require('gulp-sourcemaps');
 
 var styleSRC = './src/sass/**/*.scss';
-var styleDIST = './dist/css';
+var destination = './dist';
 
-gulp.task('expanded', async function(cb){
+gulp.task('purge', async function() {
+    return gulp.src('dist/', { read: false, allowEmpty: true }).pipe(clean());
+});
+
+gulp.task('copy-images', async function() {
+    return gulp.src(['src/images/**']).pipe(wait(1000)).pipe(gulp.dest(`${destination}/images`));
+});
+
+gulp.task('copy-fonts', async function() {
+    return gulp.src(['src/fonts/**/*']).pipe(wait(1000)).pipe(gulp.dest(`${destination}/fonts`));
+})
+
+gulp.task('expanded-styles', async function(cb) {
     gulp.src(styleSRC)
         .pipe(sass({
             errorLogToConsole: true,
@@ -16,12 +30,11 @@ gulp.task('expanded', async function(cb){
             includePaths: ['./node_modules/']
         }))
         .on('error', console.error.bind(console))
-        .pipe(gulp.dest(styleDIST))
+        .pipe(gulp.dest(`${destination}/css`))
         .on('end', cb);
-
 });
 
-gulp.task('compressed', function(cb){
+gulp.task('compressed-styles', async function(cb) {
     gulp.src( styleSRC )
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -36,13 +49,16 @@ gulp.task('compressed', function(cb){
         // }))
         .pipe(rename( { suffix: '.min' } ) )
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(styleDIST))
+        .pipe(gulp.dest(`${destination}/css`))
         .on('end', cb);
-
 });
 
-gulp.task('compile', gulp.series('expanded', 'compressed'));
+gulp.task('expanded', gulp.series('purge', 'copy-images', 'copy-fonts', 'expanded-styles'));
 
-gulp.task('default', function(){
-    gulp.watch(styleSRC).on('change', gulp.series('expanded', 'compressed'));
+gulp.task('compressed', gulp.series('purge', 'copy-images', 'copy-fonts', 'compressed-styles'));
+
+gulp.task('compile', gulp.series('purge', 'copy-images', 'copy-fonts', 'expanded-styles', 'compressed-styles'));
+
+gulp.task('default', function() {
+    gulp.watch(styleSRC).on('change', gulp.series('expanded-styles', 'compressed-styles'));
 });
